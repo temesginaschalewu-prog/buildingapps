73df4d2fa2b8ad7f85ab15d5fd79f2f906bb5c71
kalaabalb <alebachewkalaab99@gmail.com>
2026-03-06 13:12:17 +0300

    Build category card with status badges and images

--- lib/widgets/category/category_card.dart
+++ lib/widgets/category/category_card.dart
@@ -1,29 +1,17 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:provider/provider.dart';
-
 import '../../models/category_model.dart';
-import '../../providers/subscription_provider.dart';
-import '../../providers/auth_provider.dart';
 import '../../themes/app_colors.dart';
 import '../../themes/app_text_styles.dart';
 import '../../themes/app_themes.dart';
-import '../../utils/responsive.dart';
 import '../../utils/responsive_values.dart';
 import '../../utils/ui_helpers.dart';
-import '../../utils/app_enums.dart';
 import '../common/app_card.dart';
-import '../common/app_shimmer.dart';
-import '../common/responsive_widgets.dart';
 
 class CategoryCard extends StatelessWidget {
   final Category category;
   final bool hasSubscription;
-  final bool isCheckingSubscription;
-  final bool hasCachedData;
-  final bool isRefreshInProgress;
   final VoidCallback? onTap;
   final int index;
 
@@ -31,9 +19,6 @@ class CategoryCard extends StatelessWidget {
     super.key,
     required this.category,
     required this.hasSubscription,
-    this.isCheckingSubscription = false,
-    this.hasCachedData = false,
-    this.isRefreshInProgress = false,
     this.onTap,
     this.index = 0,
   });
@@ -54,10 +39,9 @@ class CategoryCard extends StatelessWidget {
           child: Text(
             category.initials,
             style: TextStyle(
-              color: Colors.white,
-              fontSize: imageFontSize,
-              fontWeight: FontWeight.bold,
-            ),
+                color: Colors.white,
+                fontSize: imageFontSize,
+                fontWeight: FontWeight.bold),
           ),
         ),
       );
@@ -78,10 +62,9 @@ class CategoryCard extends StatelessWidget {
           child: Text(
             category.initials,
             style: TextStyle(
-              color: Colors.white,
-              fontSize: imageFontSize,
-              fontWeight: FontWeight.bold,
-            ),
+                color: Colors.white,
+                fontSize: imageFontSize,
+                fontWeight: FontWeight.bold),
           ),
         ),
       ),
@@ -97,10 +80,9 @@ class CategoryCard extends StatelessWidget {
           child: Text(
             category.initials,
             style: TextStyle(
-              color: Colors.white,
-              fontSize: imageFontSize,
-              fontWeight: FontWeight.bold,
-            ),
+                color: Colors.white,
+                fontSize: imageFontSize,
+                fontWeight: FontWeight.bold),
           ),
         ),
       ),
@@ -109,7 +91,7 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isDesktop = ScreenSize.isDesktop(context);
+    final isDesktop = MediaQuery.of(context).size.width >= 1024;
     final titleSize = ResponsiveValues.categoryCardTitleSize(context);
     final priceSize = ResponsiveValues.categoryCardPriceSize(context);
     final badgeTextSize = ResponsiveValues.categoryCardBadgeTextSize(context);
@@ -146,9 +128,7 @@ class CategoryCard extends StatelessWidget {
               BorderRadius.circular(ResponsiveValues.radiusXLarge(context)),
           child: Stack(
             children: [
-              Positioned.fill(
-                child: _buildImage(context),
-              ),
+              Positioned.fill(child: _buildImage(context)),
               Positioned.fill(
                 child: Container(
                   decoration: BoxDecoration(
@@ -186,10 +166,9 @@ class CategoryCard extends StatelessWidget {
                       color: Colors.white,
                       shadows: [
                         Shadow(
-                          color: Colors.black.withValues(alpha: 0.5),
-                          blurRadius: 4,
-                          offset: const Offset(0, 2),
-                        ),
+                            color: Colors.black.withValues(alpha: 0.5),
+                            blurRadius: 4,
+                            offset: const Offset(0, 2))
                       ],
                     ),
                     maxLines: 1,
@@ -227,9 +206,7 @@ class CategoryCard extends StatelessWidget {
                 child: Container(
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.white.withValues(alpha: 0.1),
-                      width: 0.5,
-                    ),
+                        color: Colors.white.withValues(alpha: 0.1), width: 0.5),
                     borderRadius: BorderRadius.circular(
                         ResponsiveValues.radiusXLarge(context)),
                   ),
@@ -242,13 +219,13 @@ class CategoryCard extends StatelessWidget {
     )
         .animate()
         .fadeIn(
-          duration: AppThemes.animationDurationMedium,
+          duration: AppThemes.animationMedium,
           delay: Duration(milliseconds: index * 50),
         )
         .scale(
           begin: const Offset(0.95, 0.95),
           end: const Offset(1, 1),
-          duration: AppThemes.animationDurationMedium,
+          duration: AppThemes.animationMedium,
           delay: Duration(milliseconds: index * 50),
         );
   }
@@ -262,37 +239,26 @@ class CategoryCard extends StatelessWidget {
     required double textSize,
     required double padding,
   }) {
-    final isDesktop = ScreenSize.isDesktop(context);
+    final isDesktop = MediaQuery.of(context).size.width >= 1024;
 
     return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: padding,
-        vertical: padding / 2,
-      ),
+      padding: EdgeInsets.symmetric(horizontal: padding, vertical: padding / 2),
       decoration: BoxDecoration(
         color: color.withValues(alpha: 0.2),
         borderRadius:
             BorderRadius.circular(ResponsiveValues.radiusSmall(context)),
-        border: Border.all(
-          color: color.withValues(alpha: 0.4),
-          width: 0.5,
-        ),
+        border: Border.all(color: color.withValues(alpha: 0.4), width: 0.5),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withValues(alpha: 0.2),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
+              color: Colors.black.withValues(alpha: 0.2),
+              blurRadius: 4,
+              offset: const Offset(0, 2))
         ],
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(
-            icon,
-            size: iconSize,
-            color: Colors.white,
-          ),
+          Icon(icon, size: iconSize, color: Colors.white),
           SizedBox(width: isDesktop ? 2 : 4),
           Text(
             label,
@@ -302,10 +268,9 @@ class CategoryCard extends StatelessWidget {
               fontWeight: FontWeight.w600,
               shadows: [
                 Shadow(
-                  color: Colors.black.withValues(alpha: 0.3),
-                  blurRadius: 2,
-                  offset: const Offset(0, 1),
-                ),
+                    color: Colors.black.withValues(alpha: 0.3),
+                    blurRadius: 2,
+                    offset: const Offset(0, 1))
               ],
             ),
           ),
@@ -320,37 +285,27 @@ class CategoryCard extends StatelessWidget {
     required double smallTextSize,
     required double padding,
   }) {
-    final isDesktop = ScreenSize.isDesktop(context);
+    final isDesktop = MediaQuery.of(context).size.width >= 1024;
 
     return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: padding,
-        vertical: padding / 2,
-      ),
+      padding: EdgeInsets.symmetric(horizontal: padding, vertical: padding / 2),
       decoration: BoxDecoration(
         color: AppColors.telegramBlue.withValues(alpha: 0.2),
         borderRadius:
             BorderRadius.circular(ResponsiveValues.radiusSmall(context)),
         border: Border.all(
-          color: AppColors.telegramBlue.withValues(alpha: 0.4),
-          width: 0.5,
-        ),
+            color: AppColors.telegramBlue.withValues(alpha: 0.4), width: 0.5),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withValues(alpha: 0.2),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
+              color: Colors.black.withValues(alpha: 0.2),
+              blurRadius: 4,
+              offset: const Offset(0, 2))
         ],
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Icon(
-            Icons.attach_money_rounded,
-            size: 8,
-            color: Colors.white,
-          ),
+          const Icon(Icons.attach_money_rounded, size: 8, color: Colors.white),
           SizedBox(width: isDesktop ? 1 : 2),
           Text(
             category.price!.toStringAsFixed(0),
@@ -360,10 +315,9 @@ class CategoryCard extends StatelessWidget {
               fontWeight: FontWeight.w600,
               shadows: [
                 Shadow(
-                  color: Colors.black.withValues(alpha: 0.3),
-                  blurRadius: 2,
-                  offset: const Offset(0, 1),
-                ),
+                    color: Colors.black.withValues(alpha: 0.3),
+                    blurRadius: 2,
+                    offset: const Offset(0, 1))
               ],
             ),
           ),
@@ -374,53 +328,14 @@ class CategoryCard extends StatelessWidget {
               color: Colors.white.withValues(alpha: 0.8),
               shadows: [
                 Shadow(
-                  color: Colors.black.withValues(alpha: 0.3),
-                  blurRadius: 2,
-                  offset: const Offset(0, 1),
-                ),
+                    color: Colors.black.withValues(alpha: 0.3),
+                    blurRadius: 2,
+                    offset: const Offset(0, 1))
               ],
             ),
           ),
         ],
       ),
     );
-  }
-}
-
-class CategoryCardShimmer extends StatelessWidget {
-  final int index;
-
-  const CategoryCardShimmer({super.key, this.index = 0});
-
-  @override
-  Widget build(BuildContext context) {
-    ScreenSize.isDesktop(context);
-    final cardHeight = ResponsiveValues.categoryCardHeight(context);
-    ResponsiveValues.spacingM(context);
-    ResponsiveValues.categoryCardTitleSize(context);
-    ResponsiveValues.categoryCardPriceSize(context);
-
-    return Container(
-      height: cardHeight,
-      decoration: BoxDecoration(
-        color: Theme.of(context).brightness == Brightness.dark
-            ? AppColors.darkSurface
-            : AppColors.lightSurface,
-        borderRadius:
-            BorderRadius.circular(ResponsiveValues.radiusXLarge(context)),
-        border: Border.all(
-          color: Theme.of(context).brightness == Brightness.dark
-              ? AppColors.darkDivider.withValues(alpha: 0.3)
-              : AppColors.lightDivider.withValues(alpha: 0.3),
-        ),
-      ),
-      child: AppShimmer(
-        type: ShimmerType.categoryCard,
-        index: index,
-      ),
-    ).animate().fadeIn(
-          duration: AppThemes.animationDurationMedium,
-          delay: Duration(milliseconds: index * 50),
-        );
   }
 }
